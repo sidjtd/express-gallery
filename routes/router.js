5c28b1de30e701520a568c183ff659f0c3019e55
panicboy <asasaki@gmail.com>
2016-07-22 11:58:43 -1000

    POST & GET working, able to insert & query DB

--- routes/router.js
+++ routes/router.js
@@ -13,56 +13,79 @@ Router.get('/', (req, res) => {
     });
 });
 
-Router.get('/gallery/:id/', (req, res) => {
-  Post.byId(req.params.id)
-    .then( _ => {
-      return res.status(200).send({'success': true})})
-    .catch( err => {
-      return res.status(500).send({'success': false});
+Router.get('/new', (req, res) => {
+  res.send('GET received for /gallery/new');
+  // return res.render('./products/new');
+});
+
+Router.get('/:id', (req, res) => {
+  var id = req.params.id;
+  Post.findAll({
+    where: {
+      id: `${id}`
+    }
+  })
+  .then(function (postdata) {
+      res.json(postdata); // returns query result
     });
+  // Post.byId(req.params.id)
+  //   .then( _ => {
+  //     return res.status(200).send({'success': true})})
+  //   .catch( err => {
+  //     return res.status(500).send({'success': false});
+  //   });
 });
 
 
-Router.get('/gallery/new', (req, res) => {
-  return res.render('./products/new');
+
+Router.post('/', (req,res)=>{
+  Post.create(req.body)
+  .then(function (postdata) {
+      res.json(postdata); // sends back values as entered into DB
+    });
+  //res.send('POST received for /gallery');
+  // Post.add(req.body)
+  // .then( _ => {
+  //   return res.status(200).send({'success': true})})
+  // .catch( err => {
+  //   return res.status(500).send({'success': false});
+  // });
 });
 
-
-Router.post('/gallery', (req,res)=>{
-  Post.add(req.body)
-  .then( _ => {
-    return res.status(200).send({'success': true})})
-  .catch( err => {
-    return res.status(500).send({'success': false});
-  });
+Router.get('/:id/edit', (req, res) => {
+  var id = req.params.id;
+  res.send(`GET (edit) received for /gallery/${id}/edit`);
+  // Post.edit(req.body)
+  // .then( _ => {
+  //   return res.status(200).send({'success': true})})
+  // .catch( err => {
+  //   return res.status(500).send({'success': false});
+  // });
 });
 
-Router.get('/gallery/:id/edit', (req, res) => {
-  Post.edit(req.body)
-  .then( _ => {
-    return res.status(200).send({'success': true})})
-  .catch( err => {
-    return res.status(500).send({'success': false});
-  });
+Router.put('/:id', (req, res) => {
+  var id = req.params.id;
+  res.send(`PUT (edit) received for /gallery/${id}`);
+  // Post.put(req.param.id)
+  // .then( _ => {
+  //   return res.status(200).send({'success': true})})
+  // .catch( err => {
+  //   return res.status(500).send({'success': false});
+  // });
 });
 
-Router.put('/gallery/:id', (req, res) => {
-  Post.put(req.param.id)
-  .then( _ => {
-    return res.status(200).send({'success': true})})
-  .catch( err => {
-    return res.status(500).send({'success': false});
-  });
-});
-
-Router.delete('/gallery/:id', (req, res) => {
-  Post.delete(req.param.id)
-  .then( _ => {
-    return res.status(200).send({'success': true})})
-  .catch( err => {
-    return res.status(500).send({'success': false});
-  });
+Router.delete('/:id', (req, res) => {
+  var id = req.params.id;
+  res.send(`DELETE received for /gallery/${id}`);
+  // Post.delete(req.param.id)
+  // .then( _ => {
+  //   return res.status(200).send({'success': true})})
+  // .catch( err => {
+  //   return res.status(500).send({'success': false});
+  // });
 });
 
 
 module.exports = Router;
+
+
