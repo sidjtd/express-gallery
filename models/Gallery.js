28bb2616b60f0641bd7abf34f4738d4117dab456
panicboy <asasaki@gmail.com>
2016-07-22 09:12:29 -1000

    changed DB name in sql setup

---
+++ models/Gallery.js
@@ -0,0 +1,15 @@
+// module.exports = function(sequelize, DataTypes) {
+//   var Gallery = sequelize.define("Gallery", {
+//     author: DataTypes.STRING,
+//     link: DataTypes.STRING,
+//     description: Datatypes.TEXT
+//   }, {
+//     classMethods: {
+//       associate: function(models) {
+//         Gallery.hasMany(models.Task)
+//       }
+//     }
+//   });
+
+//   return Gallery;
+// };
