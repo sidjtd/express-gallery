206b6b4d5d6c22cd14c3fe6153adf9c89c41e08e
panicboy <asasaki@gmail.com>
2016-07-22 14:19:28 -1000

    Routes working, returning results from DB. Able to insert, update, find, delete

--- routes/router.js
+++ routes/router.js
@@ -5,11 +5,9 @@ const db = require('../models');
 const Post = db.Post;
 
 Router.get('/', (req, res) => {
-  Post.all()
-    .then( _ => {
-      return res.status(200).send({'success': true})})
-    .catch( err => {
-      return res.status(500).send({'success': false});
+  Post.findAll()
+  .then(function (findResult) {
+      res.json(findResult); // returns query result as an array of objects
     });
 });
 
@@ -20,71 +18,49 @@ Router.get('/new', (req, res) => {
 
 Router.get('/:id', (req, res) => {
   var id = req.params.id;
-  Post.findAll({
-    where: {
-      id: `${id}`
-    }
-  })
-  .then(function (postdata) {
-      res.json(postdata); // returns query result
+  Post.findById(id)
+  .then(function (findResult) {
+      res.json(findResult); // returns query result as a single object
     });
-  // Post.byId(req.params.id)
-  //   .then( _ => {
-  //     return res.status(200).send({'success': true})})
-  //   .catch( err => {
-  //     return res.status(500).send({'success': false});
-  //   });
 });
-
-
 
 Router.post('/', (req,res)=>{
   Post.create(req.body)
   .then(function (postdata) {
       res.json(postdata); // sends back values as entered into DB
     });
-  //res.send('POST received for /gallery');
-  // Post.add(req.body)
-  // .then( _ => {
-  //   return res.status(200).send({'success': true})})
-  // .catch( err => {
-  //   return res.status(500).send({'success': false});
-  // });
 });
 
 Router.get('/:id/edit', (req, res) => {
   var id = req.params.id;
-  res.send(`GET (edit) received for /gallery/${id}/edit`);
-  // Post.edit(req.body)
-  // .then( _ => {
-  //   return res.status(200).send({'success': true})})
-  // .catch( err => {
-  //   return res.status(500).send({'success': false});
-  // });
+  Post.findById(id)
+  .then(function (findResult) {
+      res.json(findResult); // returns query result as a single object
+    });
+  // populate rendered form values with findResult.author, findResult.image_url,
+  // findResult.link, findResult.description
 });
 
 Router.put('/:id', (req, res) => {
-  var id = req.params.id;
-  res.send(`PUT (edit) received for /gallery/${id}`);
-  // Post.put(req.param.id)
-  // .then( _ => {
-  //   return res.status(200).send({'success': true})})
-  // .catch( err => {
-  //   return res.status(500).send({'success': false});
-  // });
+  var body = req.body;
+  body.id = req.params.id;
+  Post.upsert(body)
+  .then(function (result) {
+      res.json(result); // sends back 0 or 1 for failure/success
+    });
 });
 
 Router.delete('/:id', (req, res) => {
   var id = req.params.id;
-  res.send(`DELETE received for /gallery/${id}`);
-  // Post.delete(req.param.id)
-  // .then( _ => {
-  //   return res.status(200).send({'success': true})})
-  // .catch( err => {
-  //   return res.status(500).send({'success': false});
-  // });
+  Post.destroy({
+    where: {
+      id: `${id}`
+    }
+  })
+  .then(function (result) {
+      res.json(result); // sends back 0 or 1 for failure/success
+    });
 });
-
 
 module.exports = Router;
 
