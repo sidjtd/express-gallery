1057db00afa0c202ebe5721b1ccbbd15007b7145
panicboy <asasaki@gmail.com>
2016-07-26 11:33:24 -1000

    config auth moved into /config/config.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./public"
+      // ,
+      // proxy: 'localhost:3000'
+  });
+  gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch("scss/**/*.scss", ['sass']);
+  gulp.watch("public/*").on('change', browserSync.reload);
+});
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  browserSync.init({
-    server: "./public",
-      proxy: 'localhost:3000/'
-    });
   return gulp.src("scss/styles.scss")
     .pipe(sass())
     .pipe(gulp.dest("public/css"))
