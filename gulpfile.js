861c9998d3fd9e4f3bdc869f378b7a27ebbb6f85
SDodson <siddodson@gmail.com>
2016-07-26 11:37:01 -1000

    Pre-merge commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,25 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-<<<<<<< HEAD
-  // browserSync.init({
-  //     server: "./public",
-  //       proxy: 'localhost:3000'
-  // });
+  browserSync.init({
+      server: "./public"
+      // ,
+      // proxy: 'localhost:3000'
+  });
   gulp.watch("scss/*.scss", ['sass']);
-  gulp.watch("scss/*/*.scss", ['sass']);
+  gulp.watch("scss/**/*.scss", ['sass']);
   gulp.watch("public/*").on('change', browserSync.reload);
 });
 
-=======
->>>>>>> andrew_master
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  browserSync.init({
-    server: "./public",
-      proxy: 'localhost:3000/'
-    });
   return gulp.src("scss/styles.scss")
     .pipe(sass())
     .pipe(gulp.dest("public/css"))
     .pipe(browserSync.stream());
 });
 
-<<<<<<< HEAD
 gulp.task('default', ['serve']);
-=======
-gulp.task('default', ['serve']);
->>>>>>> andrew_master
